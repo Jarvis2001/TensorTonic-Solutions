e14c9d34a3a1b30a6fbba6b290feb49b5df282e7
Makarand More <40858007+Jarvis2001@users.noreply.github.com>
2026-09-13 18:14:47 +0530

    Solve matrix-addition

--- study-plans/cuda-basics/matrix-addition/matrix-addition.cu
+++ study-plans/cuda-basics/matrix-addition/matrix-addition.cu
@@ -1,33 +1,13 @@
 #include <cuda_runtime.h>
 
 __global__ void matrix_add_kernel(const float* A, const float* B, float* C, int M, int N) {
-    //Well the previous one betrayed me
+    //Shortest one yet
+    int tid = (blockIdx.y * gridDim.x + blockIdx.x) * (blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x + threadIdx.x);
     int total_elements = M * N;
 
-    if (total_elements % 4 == 0) {
-        const float4* A4 = reinterpret_cast<const float4*>(A);
-        const float4* B4 = reinterpret_cast<const float4*>(B);
-        float4* C4 = reinterpret_cast<float4*>(C);
-    
-        int total_vec = total_elements / 4;
-        int tid = (blockIdx.y * gridDim.x + blockIdx.x) * (blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x + threadIdx.x);
-    
-        //Direct mapping without loop overhead
-        if (tid  < total_vec) {
-            float4 a = A4[tid];
-            float4 b = B4[tid];
-            C4[tid] = make_float4(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
-        }        
-    } else {
-        int col = blockIdx.x * blockDim.x + threadIdx.x;
-        int row = blockIdx.y * blockDim.y + threadIdx.y;
-
-        if (row < M && col < N) {
-            int idx = row * N + col;
-            C[idx] = A[idx] + B[idx];            
-        }
+    if (tid < total_elements) {
+        C[tid] = A[tid] + B[tid];
     }
-    
 }
 
 extern "C" void solve(const float* A, const float* B, float* C, int M, int N) {
